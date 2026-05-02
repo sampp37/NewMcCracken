ebb6f3f78c50f33db10a461442bc5dfa0c6f45b8
sampp37 <sampp37@gmail.com>
2026-05-02 01:28:27 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -656,6 +656,7 @@ function App() {
 
 
       {/* Contact Section */}
+      <div id="FreeEstimate" />
       <section id="contact" className="py-20 bg-white">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-12">
