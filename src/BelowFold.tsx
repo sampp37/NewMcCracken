e430af3ecb94dd3b3cd9695e2f3a48d18ad33df1
sampp37 <sampp37@gmail.com>
2026-07-13 21:28:38 -0500

    Updated BelowFold.tsx

--- src/BelowFold.tsx
+++ src/BelowFold.tsx
@@ -417,7 +417,7 @@ export default function BelowFold({ openModal }: { openModal: () => void }) {
                 alt: 'Deck painting service Lafayette Indiana',
                 text: <><strong>Prepping</strong> the wood right takes attention, and we don't rush it. Once we've wrapped up, cleanup is just as thorough.</>,
                 imgTall: true,
-                imgInset: true,
+                imgContain: true,
               },
               {
                 title: 'Garage Epoxy Floor',
